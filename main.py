835ffd0097ce5f6b1665f6f05fb41cd36253ab53
SALIM UMER <salimbinumer@gmail.com>
2023-04-04 17:38:19 +0530

    combo box

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class MainWindow(qtw.QWidget):
         self.setLayout(qtw.QVBoxLayout())  #vertical v and horizontall H
         
         # craete label
-        my_label = qtw.QLabel("hello world what is the your name")
+        my_label = qtw.QLabel("select the port for communication")
         # chhange the font size
         my_label.setFont(qtg.QFont('helvetica',19))
         
@@ -22,15 +22,33 @@ class MainWindow(qtw.QWidget):
         self.layout().addWidget(my_label)
         
          
-        #entry the box
-        my_entry = qtw.QLineEdit()
-        my_entry.setObjectName("name_filed")
-        my_entry.setText("")
-        self.layout().addWidget(my_entry) 
+        # #entry the box replace to combo box
+        # my_entry = qtw.QLineEdit()
+        # my_entry.setObjectName("name_filed")
+        # my_entry.setText("")
+        # self.layout().addWidget(my_entry) 
+        
+        
+        # create the combo box 
+        
+        my_combo = qtw.QComboBox(self,editable=True,insertPolicy=qtw.QComboBox.InsertAtTop)
+        # add  item to combo box
+        
+        my_combo.addItem("port1","something")
+        my_combo.addItem("port2",2)
+        my_combo.addItem("port3",qtw.QWidget)
+        my_combo.addItem("port4")
+        my_combo.addItem("por5")
+        my_combo.addItem("port6")
+        my_combo.addItems(["one","two"])
+        my_combo.addItems(2,"Third thing")
+        # put the combo on the screen
+        self.layout().addWidget(my_combo)
+        
         
         
         # create the button
-        my_button = qtw.QPushButton("press me!", clicked= lambda:press_it())
+        my_button = qtw.QPushButton("press me!", clicked= lambda:press_it(self))
         self.layout().addWidget(my_button) 
 
         
@@ -39,8 +57,10 @@ class MainWindow(qtw.QWidget):
         
         
         def press_it(self):
-            my_label.setText(f'hello{my_entry.text}')
-            my_entry.setText("")
+            # add name to label
+            
+            my_label.setText(f'selcted {my_combo.currentText()}!')      #text - for giving same output #data - for specific data from us
+            
 
         
         
